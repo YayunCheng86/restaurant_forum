6ac9626002e54db2cd003a5bab0f32bac33e05ff
Laura <lolacheng86@gmail.com>
2020-05-07 13:32:34 +0800

    Create category model and add restaurant.CategoryId and association

---
+++ migrations/20200506142341-add-categoryId-to-restaurant.js
@@ -0,0 +1,18 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.addColumn('restaurants', 'CategoryId', { 
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        reference: {
+          model: 'categories',
+          key: 'id'
+        } 
+      });
+  },
+
+  down: (queryInterface, Sequelize) => {
+      return queryInterface.removeColumn('Restaurants', 'Categories');
+  }
+};
