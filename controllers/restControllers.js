c27d596314ba8cec0b74ff8b04a3de51b93f6004
Laura <lolacheng86@gmail.com>
2020-05-08 18:36:07 +0800

    Add categories navbar on restaurants index page, add filter on category navbar and Show active item on category navbars

--- controllers/restControllers.js
+++ controllers/restControllers.js
@@ -1,6 +1,42 @@
-const restController = {
+const db = require('../models')
+const Restaurant = db.Restaurant
+const Category = db.Category
+let restController = {
     getRestaurants: (req, res) => {
-        return res.render('restaurants')
+        let whereQuery = {}
+        let categoryId = ''
+        if (req.query.categoryId) {
+            categoryId = Number(req.query.categoryId)
+            whereQuery['CategoryId'] = categoryId
+        }
+
+        Restaurant.findAll({ include: Category, where: whereQuery }).then(restaurants => {
+            const data = restaurants.map(r => ({
+                ...r.dataValues,
+                description: r.dataValues.description.substring(0, 50),
+                categoryName: r.Category.name
+            }))
+
+            Category.findAll({
+                raw: true,
+                nest: true
+            }).then(categories => { // 取出 categoies 
+                return res.render('restaurants', {
+                    restaurants: data,
+                    categories: categories,
+                    categoryId: categoryId
+                })
+            })
+        })
+    },
+    getRestaurant: (req, res) => {
+        return Restaurant.findByPk(req.params.id, {
+            include: Category
+        }).then(restaurant => {
+            return res.render('restaurant', {
+                restaurant: restaurant.toJSON()
+            })
+        })
     }
 }
 
--- routes/index.js
+++ routes/index.js
@@ -40,6 +40,7 @@ module.exports = (app, passport) => {
     app.get('/admin/categories/:id', authenticatedAdmin, categoryController.getCategories)
     app.put('/admin/categories/:id', authenticatedAdmin, categoryController.putCategory)
     app.delete('/admin/categories/:id', authenticatedAdmin, categoryController.deleteCategory)
+    app.get('/restaurants/:id', authenticated, restController.getRestaurant)
     
     // 註冊
     app.get('/signup', userController.signUpPage)
