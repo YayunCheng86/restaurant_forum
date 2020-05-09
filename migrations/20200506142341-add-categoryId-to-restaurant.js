b0a8f1c93a1196c52befa22debc9bd47aafeadf4
Laura <lolacheng86@gmail.com>
2020-05-09 20:57:55 +0800

    Update restaurants migration

--- migrations/20200506142341-add-categoryId-to-restaurant.js
+++ migrations/20200506142341-add-categoryId-to-restaurant.js
@@ -13,6 +13,6 @@ module.exports = {
   },
 
   down: (queryInterface, Sequelize) => {
-      return queryInterface.removeColumn('Restaurants', 'Categories');
+    return queryInterface.removeColumn('Restaurants', 'CategoryId');
   }
 };
