2674708fd53e5a24573669321e80ca41800576df
Laura <lolacheng86@gmail.com>
2020-05-15 08:35:29 +0800

    DELETE /api/admin/restaurants/:id and refactor adminController.deleteRestaurant

--- routes/apis.js
+++ routes/apis.js
@@ -4,8 +4,10 @@ const router = express.Router()
 const adminController = require('../controllers/api/adminController.js')
 const categoryController = require('../controllers/api/categoryController')
 
+// 後台
 router.get('/admin/restaurants', adminController.getRestaurants)
 router.get('/admin/restaurants/:id', adminController.getRestaurant)
 router.get('/admin/categories', categoryController.getCategories)
+router.delete('/admin/restaurants/:id', adminController.deleteRestaurant)
 
 module.exports = router
