ef99e91a7556c2ac7fd2af30d13866bdea2889fe
Laura <lolacheng86@gmail.com>
2020-05-15 11:14:27 +0800

    PUT /api/admin/categories/:id

--- controllers/api/categoryController.js
+++ controllers/api/categoryController.js
@@ -13,6 +13,11 @@ let categoryController = {
             return res.json(data)
         })
     },
+    putCategory: (req, res) => {
+        categoryService.putCategory(req, res, (data) => {
+            return res.json(data)
+        })
+    },
 }
 
 module.exports = categoryController
